819324eb51ce2ad0aca1191ad69bd3d4579a0b57
Hamza El Alaoui <62178890+hamzaeIalaoui@users.noreply.github.com>
2023-04-12 00:00:33 +0000

    added gamified sound for button clicks

--- src/App.js
+++ src/App.js
@@ -5,39 +5,50 @@ import ExplanationPage from "./ExplanationPage";
 import ChatScreen from "./ChatScreen";
 import LanguageSelector from "./LanguageSelector";
 
+import clickSound from  "./clickSound.wav";
 
 function App() {
   const [currentPage, setCurrentPage] = useState(0);
   const [language, setLanguage] = useState("Darija_ar");
 
-  const handleNext = () => setCurrentPage(currentPage + 1);
-  const handlePrevious = () => setCurrentPage(currentPage - 1);
+  const handleNext = () => {
+    setCurrentPage(currentPage + 1);
+    const audio = new Audio(clickSound); // create new Audio instance
+    audio.play(); // play the click sound
+  };
+
+  const handlePrevious = () => {
+    setCurrentPage(currentPage - 1);
+    const audio = new Audio(clickSound); // create new Audio instance
+    audio.play(); // play the click sound
+  };
+
   const handleLanguageChange = (newLanguage) => {
     setLanguage(newLanguage);
   };
 
   return (
     <div className="App">
+      <audio id="click-sound">
+        <source src={clickSound} type="audio/wav" />
+      </audio>
       <LanguageSelector onLanguageChange={handleLanguageChange} />
 
+      {currentPage === 0 && (
+        <IntroPage onNext={handleNext} language={language} />
+      )}
 
-      {currentPage === 0 && <IntroPage
-        onNext={handleNext} language={language} />}
+      {currentPage === 1 && (
+        <ExplanationPage
+          onNext={handleNext}
+          onPrevious={handlePrevious}
+          language={language}
+        />
+      )}
 
-
-      {currentPage === 1 && (<ExplanationPage
-        onNext={() => setCurrentPage(currentPage + 1)}
-        onPrevious={() => setCurrentPage(currentPage - 1)}
-        language={language}
-      />)}
-
-
-      {currentPage === 2 && <ChatScreen
-        onPrevious={() => setCurrentPage(currentPage - 1)}  language={language}   />
-
-
-
-      }
+      {currentPage === 2 && (
+        <ChatScreen onPrevious={handlePrevious} language={language} />
+      )}
     </div>
   );
 }
