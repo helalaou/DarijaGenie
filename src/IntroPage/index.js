a26a55b8d6b8e30d74c86efc319676a23ec6b409
Hamza El Alaoui <62178890+hamzaeIalaoui@users.noreply.github.com>
2023-04-13 22:43:25 +0000

    added floating animation for the genie, added appearing animation, added a magical teapot, added shadow, added a traditional moroccan background, +other

--- src/IntroPage/index.js
+++ src/IntroPage/index.js
@@ -1,59 +1,100 @@
-import React from "react";
 import { getButtonTexts } from "../LanguageSelector";
 import styles from "./styles.module.css";
+import React, { useState, useEffect } from "react";
+
 
 const IntroPage = ({ onNext, language }) => {
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
   const buttonTexts = getButtonTexts(language);
 
 
-  const getText = () => {
-    
+  useEffect(() => {
+    if (isFirstLoad) {
+      setTimeout(() => {
+        setIsFirstLoad(false);
+      }, 1000); // Duration of the genieAppearing animation
+    }
+  }, [isFirstLoad]);
+
+
+  const getGreeting = () => {
     if (language === "Darija_ar") {
-      return {
-        title: "سلام, أنا منير, جني الدارجة",
-        subtitle: "أنا هنا باش نعلمك الدارجة بطريقة ساهلة وممتعة",
-      };
+      return "سلام";
+
+    } else if (language === "Darija_roman") {
+      return "Salam";
+
+    } else if (language === "English") {
+      return "Hello";
+
+    } else if (language === "French") {
+      return "Bonjour";
     }
-
-    else if (language === "Darija_roman") {
-      return {
-        title: "Salam, ana Mounir, Gini Darija",
-        subtitle: "Ana hna bach n3almek darija b tariqa sahla wa momti3a",
-      };
-    }
-
-    else if (language === "English") {
-      return {
-        title: "Hello, I am Mounir, the Darija Genie",
-        subtitle: "I am here to teach you Darija in a simple and fun way",
-      };
-    }
-    else if (language === "French") {
-      return {
-        title: "Bonjour, je suis Mounir, le génie Darija",
-        subtitle: "Je suis ici pour vous apprendre le Darija d'une manière simple et amusante",
-
-      }
-    }
-
-
   };
 
-  const text = getText();
+  const getTitle = () => {
+    if (language === "Darija_ar") {
+      return "أنا هنا لتعليم الدارجة بطريقة سهلة وممتعة";
+
+    } else if (language === "Darija_roman") {
+      return "Ana hna bach n3almek darija b tariqa sahla wa momti3a";
+
+    } else if (language === "English") {
+      return "I am here to teach you Darija in a simple and fun way";
+
+    } else if (language === "French") {
+      return "Je suis ici pour vous apprendre le Darija d'une manière simple et amusante";
+    }
+  };
+
+  const getSubtitle = () => {
+    if (language === "Darija_ar") {
+      return "أنا سأساعدك على تعلم الدارجة بطريقة سهلة وممتعة";
+    } else if (language === "Darija_roman") {
+
+      return "Ana sa3di bik n3almek darija b tariqa sahla wa momti3a";
+
+    } else if (language === "English") {
+      return "Ready to learn Moroccan Dialect in a fun and easy way? Let's get started! ";
+
+    } else if (language === "French") {
+      return "Je vais vous aider à apprendre le Darija d'une manière simple et amusante";
+    }
+  };
+
+
+  const greeting = getGreeting();
+  const title = getTitle();
+  const subtitle = getSubtitle();
 
   return (
-    <div className={styles.container}>
-      <img className={styles.logo} src="Logo.png" alt="DarijaGenie Logo" />
-      <div className={styles.textContainer}>
-        <h1 className={styles.title}>{text.title}</h1>
-        <h1 className={styles.subtitle}>{text.subtitle}</h1>
+    <div className={`${styles.container} ${styles.backgroundHeader}`}>
+      <img
+        className={`${styles.logo} ${!isFirstLoad ? styles.logoFloating : styles.logoAppearing}`}
+        src="Logo.png"
+        alt="DarijaGenie Logo"
+      />
 
+      <img
+        className={`${styles.teapot} `} src="teapotNiit.png"
+        alt="Teapot"
+      />
+
+      <img
+        className={` ${styles.shadow}`} src="shadow.png"
+        alt="Shadow"
+      />
+
+      <div className={styles.textContainer}>
+
+        <h1 className={styles.greeting} style={{ color: "#f9c603", fontSize: "4rem" }}>{greeting}</h1>
+        <h1 className={styles.title} style={{ color: "#c92933", fontSize: "2.5rem" }}>{title}</h1>
+        <h1 className={styles.subtitle} style={{ color: "#882331", fontSize: "1.5rem" }}>{subtitle}</h1>
 
         <div className={styles.buttonContainerIntroPage}>
           <button className="button" onClick={onNext}>
             {buttonTexts.next}
           </button>
-
         </div>
       </div>
     </div>
