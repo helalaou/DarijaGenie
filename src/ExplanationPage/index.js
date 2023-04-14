b9fb3e3a19c0b828260495d8466cd1e6930973d3
Hamza El Alaoui <62178890+hamzaeIalaoui@users.noreply.github.com>
2023-04-14 20:28:18 +0000

    adjusted first page text

--- src/ExplanationPage/index.js
+++ src/ExplanationPage/index.js
@@ -9,8 +9,7 @@ const ExplanationPage = ({ onNext, onPrevious, language }) => {
   return (
     <div className={styles.container}>
       <div className="title"> 
-       This page will contain information about the pedagogical approach of the app - in other words - how to play, the available scenarios, etc.
-      </div>
+       </div>
        
       <div className={styles.buttonContainerExplanationPage} >
         <button className="button" onClick={onPrevious}>{buttonTexts.previous}</button>
--- src/IntroPage/index.js
+++ src/IntroPage/index.js
@@ -1,21 +1,16 @@
 import { getButtonTexts } from "../LanguageSelector";
-import styles from "./styles.module.css"; 
+import styles from "./styles.module.css";
 
-const IntroPage = ({ onNext, language }) => { 
+const IntroPage = ({ onNext, language }) => {
   const buttonTexts = getButtonTexts(language);
-
- 
 
   const getGreeting = () => {
     if (language === "Darija_ar") {
       return "Salam سلام";
-
     } else if (language === "Darija_roman") {
       return "Salam سلام";
-
     } else if (language === "English") {
       return "Hello مرحبا";
-
     } else if (language === "French") {
       return "Bonjour مرحبا";
     }
@@ -23,14 +18,11 @@ const IntroPage = ({ onNext, language }) => {
 
   const getTitle = () => {
     if (language === "Darija_ar") {
-      return ".أنا سلطان، جـنـي الدارجة"
-
+      return ".أنا سلطان، جـنـي الدارجة";
     } else if (language === "Darija_roman") {
       return "Ana Sutlan, jenii Darija.";
-
     } else if (language === "English") {
       return "I'm Sultan, the Darija Genie";
-
     } else if (language === "French") {
       return "Je suis Sultan, le génie du Darija.";
     }
@@ -38,40 +30,67 @@ const IntroPage = ({ onNext, language }) => {
 
   const getSubtitle = () => {
     if (language === "Darija_ar") {
-      return "!واجد تتعلم الدارجة بطريقة سهلة وممتعة؟ يلاه نبداو";
-            } else if (language === "Darija_roman") {
+      return "واجد تتعلم الدارجة بطريقة سهلة وممتعة؟  يلاه نبداو";
 
+    } else if (language === "Darija_roman") {
       return "Wajed tet3alem Darija b tari9a sahla w momte3a? Yallah nbadaw!";
-
     } else if (language === "English") {
-      return "Ready to learn Moroccan Darija in a fun and  easy way? Let's go!";
-
+      return "Ready to learn Moroccan Darija in a fun and easy way? Let's go!";
     } else if (language === "French") {
-      return "Prêt à apprendre le dialecte marocain de manière amusante et facile ? Commençons !";
+      return "Prêt à apprendre le dialecte marocain de manière amusante et facile ? Allons-y !";
     }
   };
-
 
   const greeting = getGreeting();
   const title = getTitle();
   const subtitle = getSubtitle();
 
-  return ( 
-     
-      <div className={ styles.containerIntroPage }>
-        
-       <h1 className={styles.greetingBubble}>{greeting}</h1>
+  const getTitleStyle = () => {
+    if (language === "Darija_ar") {
+      return "titlear";
+    }
+    if (language === "Darija_roman") {
+      return "titledarija";
+    }
+    if (language === "English") {
+      return "titleeng";
+    }
+    else {
+      return "titlefr";
+    }
+  }
 
-      <h1 className="title">{title}</h1>
-      <h1 className="subtitle">{subtitle}</h1>
+  const getSubtitleStyle = () => {
+    if (language === "Darija_ar") {
+      return "subtitlear";
+    }
+    if (language === "Darija_roman") {
+      return "subtitledarija";
+    }
+    if (language === "English") {
+      return "subtitleeng";
+    }
+    else {
+      return "subtitlefr";
+    }
+  }
 
-        <div className={styles.buttonContainerIntroPage}>
-          <button className="button" onClick={onNext}>
-            {buttonTexts.next}
-          </button>
-        </div> 
+
+  return (
+    <div className={styles.containerIntroPage}>
+
+      <h1 className={styles.greetingBubble}>{greeting}</h1>
+      <h1 className={styles[getTitleStyle()]}>{title}</h1>
+      <h1 className={styles[getSubtitleStyle()]}>{subtitle}</h1>
+
+
+      <div className={styles.buttonContainerIntroPage}>
+        <button className="button" onClick={onNext}>
+          {buttonTexts.next}
+        </button>
       </div>
-    
+    </div>
+
   );
 };
 
