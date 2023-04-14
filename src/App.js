d792056207ce4f4ebd4f0bdc0610a684dbbc4def
Hamza El Alaoui <62178890+hamzaeIalaoui@users.noreply.github.com>
2023-04-14 03:09:58 +0000

    feature: added an initial blurry landing page to ask user for starting such that a sound can be played -- due to browser autoplay policies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 
 import IntroPage from "./IntroPage";
@@ -6,23 +6,36 @@ import ExplanationPage from "./ExplanationPage";
 import ChatScreen from "./ChatScreen";
 import LanguageSelector from "./LanguageSelector";
 
-import clickSound from  "./clickSound.wav";
-
+import clickSound from "./clickSound.wav";
+import magicSound from "./magicSound.wav";
 
 const App = () => {
-  const [isFirstLoad, setIsFirstLoad] = useState(true);
-
-  useEffect(() => {
-    if (isFirstLoad) {
-      setTimeout(() => {
-        setIsFirstLoad(false);
-      }, 1000); // Duration of the genieAppearing animation
-    }
-  }, [isFirstLoad]);
-
-
+  const [showOverlay, setShowOverlay] = useState(true);
+  const [startAnimation, setStartAnimation] = useState(false);
   const [currentPage, setCurrentPage] = useState(0);
   const [language, setLanguage] = useState("Darija_ar");
+
+  const logoRef = useRef(null);
+
+  useEffect(() => {
+    if (logoRef.current) {
+      logoRef.current.addEventListener("animationend", (e) => {
+        if (e.animationName === "genieAppearing") {
+          logoRef.current.classList.remove("logoAppearing");
+          logoRef.current.classList.add("logoFloating");
+        }
+      });
+    }
+  }, [startAnimation]);
+
+  const handleEnter = () => {
+    const magicAudio = new Audio(magicSound);
+    magicAudio.play();
+    setShowOverlay(false);
+    setStartAnimation(true);
+  };
+
+
 
   const handleNext = () => {
     setCurrentPage(currentPage + 1);
@@ -38,45 +51,55 @@ const App = () => {
 
   const handleLanguageChange = (newLanguage) => {
     setLanguage(newLanguage);
+
   };
 
-
   return (
-
-  <div className="App">
-      <div className={`container backgroundHeader`}>
-        <img
-          className={`logo ${!isFirstLoad ? "logoFloating" : "logoAppearing"}`}
-          src="Logo.png"
-          alt="DarijaGenie Logo"
-        />
-        <img className="teapot"src="teapot.png" alt="Teapot"  />
-        <img className="shadow" src="shadow.png" alt="Shadow" />
-     
-      </div>
-    
-      <audio id="click-sound">
-        <source src={clickSound} type="audio/wav" />
-      </audio>
-      <LanguageSelector onLanguageChange={handleLanguageChange} />
-
-      {currentPage === 0 && (
-        <IntroPage onNext={handleNext} language={language} />
+    <div className="App">
+      {showOverlay && (
+        <div className="overlay">
+          <div className="enterButtonContainer">
+            <button className="enterButton" onClick={handleEnter}>
+              Enter
+            </button>
+          </div>
+        </div>
       )}
 
-      {currentPage === 1 && (
-        <ExplanationPage
-          onNext={handleNext}
-          onPrevious={handlePrevious}
-          language={language}
-        />
-      )}
+      {startAnimation && (
+        <div>
+          <div className={`container backgroundHeader`}>
+            <img
+              ref={logoRef}
+              className={`logo logoAppearing`}
+              src="Logo.png"
+              alt="DarijaGenie Logo"
+            />
+            <img className="teapot" src="teapot.png" alt="Teapot" />
+            <img className="shadow" src="shadow.png" alt="Shadow" />
+          </div>
 
-      {currentPage === 2 && (
-        <ChatScreen onPrevious={handlePrevious} language={language} />
+          <LanguageSelector onLanguageChange={handleLanguageChange} />
+
+          {currentPage === 0 && (
+            <IntroPage onNext={handleNext} language={language} />
+          )}
+
+          {currentPage === 1 && (
+            <ExplanationPage
+              onNext={handleNext}
+              onPrevious={handlePrevious}
+              language={language}
+            />
+          )}
+
+          {currentPage === 2 && (
+            <ChatScreen onPrevious={handlePrevious} language={language} />
+          )}
+        </div>
       )}
     </div>
   );
-}
+};
 
-export default App; 
+export default App;
