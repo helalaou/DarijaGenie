0e0dc2ab5e8fc792819344ea16cd25ae5b5ea7eb
Hamza El Alaoui <62178890+hamzaeIalaoui@users.noreply.github.com>
2023-04-16 22:32:23 +0000

    added a chatbot frame using MS Bot Framework

---
+++ src/ChatScreen/IframeEmbed.js
@@ -0,0 +1,12 @@
+// src/IframeEmbed.js
+import React from 'react';
+
+const IframeEmbed = ({ iframe }) => {
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: iframe ? iframe : "" }}
+    />
+  );
+};
+
+export default IframeEmbed;
--- src/ChatScreen/index.js
+++ src/ChatScreen/index.js
@@ -1,27 +1,24 @@
 import React from "react";
 import styles from "./styles.module.css";
 import { getButtonTexts } from "../LanguageSelector";
+import IframeEmbed from "./IframeEmbed";
 
+const ChatScreen = ({ onPrevious, language }) => {
+  const buttonTexts = getButtonTexts(language);
+  const iframe = `<iframe class="${styles.iframeContainer}" src='https://webchat.botframework.com/embed/darijagenie?s=C8G9pPl3IoU.f9xGFfaTn0cgUexUnqd7mNSaTWhjNPy40v96DJqXz6I'></iframe>`;
 
-const ChatScreen = ({
-    onPrevious , language}) => {
-    const buttonTexts = getButtonTexts(language);
-
-
-    return (
-        
-        <div className={styles.container}> 
-            <div className={styles.chatBox}>
-
-       
-
-            </div>
-            <div className={styles.buttonContainerChatScreen}>
-                
-                <button className="button" onClick={onPrevious}>{buttonTexts.previous}</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <div className={styles.chatBox}>
+        <IframeEmbed iframe={iframe} />
+      </div>
+      <div className={styles.buttonContainerChatScreen}>
+        <button className="button" onClick={onPrevious}>
+          {buttonTexts.previous}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default ChatScreen;
