43647ed7ed6f9a6843e4fdc54e4a82375d109123
Hamza El Alaoui <62178890+hamzaeIalaoui@users.noreply.github.com>
2023-04-19 00:47:21 +0000

    added: MS Teams popup, QRcodes, new img styling, clickable imgs new tab + other

--- src/ChatScreen/index.js
+++ src/ChatScreen/index.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import { getButtonTexts } from "../LanguageSelector";
 import IframeEmbed from "./IframeEmbed";
+import slackQRCode from './assets/QRcard-slack.jpg';
+import teamsQRCode from './assets/QRcard-teams.jpg';
+import messengerQRCode from './assets/QRcard-messenger.jpg';
+import telegramQRCode from './assets/QRcard-telegram.jpg';
+
 
 const ChatScreen = ({ onPrevious, language }) => {
   const buttonTexts = getButtonTexts(language);
@@ -56,47 +61,63 @@ const ChatScreen = ({ onPrevious, language }) => {
     <div className={styles.backgroundHeader}>
 
       <div className={styles.container}>
-        <div className={styles.chatBox}>
-          <IframeEmbed iframe={iframe} />
-        </div>
+            <div className={styles.chatBox}>
+                  <IframeEmbed iframe={iframe} />
+            </div>
+
         <div className={styles.goldenStrip}>{stripTxt}</div>
+
+
         <div className={styles.imagescontainer}>
+          <a href="https://join.slack.com/t/darijagenie/shared_invite/zt-1tqsdy4y8-YllCWJFuDaC~NZ1_QsWN_w" target="_blank" rel="noopener noreferrer" className={styles.imageLink}>
+            <figure>
+              <img src={slackQRCode} alt="Slack QR code" />
+              <figcaption className="image-text">Slack</figcaption>
+            </figure>
+          </a>
 
-           <figure>
-            <img src="https://i.imgur.com/uX6zhrd.jpg" alt="Slack QR code" />
-            <figcaption class="image-text">Slack</figcaption>
-          </figure>
+          <a href="https://t.me/DarijaGenie_bot" target="_blank" rel="noopener noreferrer" className={styles.imageLink}>
+            <figure>
+              <img src={telegramQRCode} alt="Telegram QR code" />
+              <figcaption className="image-text">Telegram</figcaption>
+            </figure>
+          </a>
 
-          <figure>
-            <img src="https://i.imgur.com/uX6zhrd.jpg" alt="Telegram QR code" />
-            <figcaption class="image-text">Telegram</figcaption>
-          </figure>
+          <a href="https://www.facebook.com/DarijaGenie" target="_blank" rel="noopener noreferrer" className={styles.imageLink}>
+            <figure>
+              <img src={messengerQRCode} alt="Messenger QR code" />
+              <figcaption className="image-text">Facebook Messenger</figcaption>
+            </figure>
+          </a>
 
-          <figure>
-            <img src="https://i.imgur.com/uX6zhrd.jpg" alt="Messenger QR code" />
-            <figcaption class="image-text">Facebook Messenger</figcaption>
-          </figure>
-
-          <figure onClick={handleMicrosoftTeamsClick}>
-          <img src="https://i.imgur.com/uX6zhrd.jpg" alt="Microsoft Teams QR code" />
-          <figcaption class="image-text">Microsoft Teams</figcaption>
-        </figure>
-          
+          <a className={styles.imageLink} onClick={handleMicrosoftTeamsClick}>
+            <figure>
+              <img src={teamsQRCode} alt="Teams QR code" />
+              <figcaption className="image-text">Microsoft Teams</figcaption>
+            </figure>
+          </a>
         </div>
+
+
+
       </div>
 
       {showPopup && (
         <div className={styles.popup}>
           <div className={styles.popupContent}>
-            <h3>Microsoft Teams</h3>
-            <p>Text for Microsoft Teams.</p>
+            <h3>Use DarijaGenie on Microsoft Teams</h3>
+            <p><strong>1.</strong> Open Microsoft Teams and sign in.</p>
+            <p><strong>2.</strong> In the search bar at the top of the application, paste the chatbot's unique ID: 54f9a37e-0055-42b3-967d-e3d2ec5c884d</p>
+            <p><strong>3.</strong> Press Enter to start the search.</p>
+            <p><strong>4.</strong> Click on the "People" tab, and the Darija chatbot should appear in the search results.</p>
+            <p><strong>5.</strong> Click on the chatbot to start or continue a conversation with it.</p>
             <button className={styles.closeButton} onClick={handleClosePopup}>
               Close
             </button>
           </div>
         </div>
       )}
- 
+
 
       <div
         className={
